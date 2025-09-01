6768f6c8bb3a5d5a19229d649c72cf70f7991a97
Ito-Hideaki <s24023@tokyo.kosen-ac.jp>
2025-09-01 11:24:59 +0900

    add two types of walls, auto-generate cave

--- Assets/Project/Scripts/Wall Generator.cs
+++ Assets/Project/Scripts/Wall Generator.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class WallGenerator : MonoBehaviour
 {
 
-    public GameObject bottomWallPrefab;
-    public GameObject minecart;
+    [SerializeField] GameObject bottomWallPrefab;
+    [SerializeField] GameObject wallPrefab;
+    [SerializeField] GameObject minecart;
 
     float lastWallZ = 0;
-    float wallGenerationLimit = 100.0f;
+    float lastBottomWallZ = 0;
+    float wallGenerationLimit = 1000f;
+    const float wallZLength = 100f;
+    const float bottomWallZLength = 50f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,14 +30,38 @@ public class WallGenerator : MonoBehaviour
         //generate walls as minecart move
         while (lastWallZ < wallGenerationLimit + minecart.transform.position.z)
         {
-            createWall();
+            createPolygonCaveWall();
+            lastWallZ += wallZLength;
+        }
+        //generate bottom walls as previous
+        while (lastBottomWallZ < wallGenerationLimit + minecart.transform.position.z)
+        {
+            createBottomCaveWall();
+            lastBottomWallZ += bottomWallZLength;
         }
     }
 
-    void createWall()
+    void createBottomCaveWall()
     {
         GameObject bottomWall = Instantiate(bottomWallPrefab) as GameObject;
-        bottomWall.transform.position = new Vector3(0, -5, lastWallZ);
-        lastWallZ += 15;
+        bottomWall.transform.position = new Vector3(0, -8, lastBottomWallZ);
+    }
+
+    void createPolygonCaveWall()
+    {
+        float angle = 0;
+        const int vertexNumber = 10;
+        for(var i = 0; i < vertexNumber; i++)
+        {
+            if (9 <= i || i <= 1) continue;
+
+            angle = (Mathf.PI / -2) + (Mathf.PI * 2 / vertexNumber) * i;
+            float radius = 20f;
+            float posX = Mathf.Cos(angle) * radius;
+            float posY = Mathf.Sin(angle) * radius;
+            GameObject wall = Instantiate(wallPrefab) as GameObject;
+            wall.transform.position = new Vector3(posX, posY, lastWallZ);
+            wall.transform.rotation = Quaternion.Euler(0, 0, i * 360 / vertexNumber);
+        }
     }
 }
