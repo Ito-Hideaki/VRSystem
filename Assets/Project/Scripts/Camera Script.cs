2951e2672bcb840f1edf49564b91a3a06db9c250
Ito-Hideaki <s24023@tokyo.kosen-ac.jp>
2025-09-18 22:52:48 +0900

    VR対応

--- Assets/Project/Scripts/Camera Script.cs
+++ Assets/Project/Scripts/Camera Script.cs
@@ -16,26 +16,5 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        const float rotationSpeed = 0.5f;
-
-        if(Input.GetKey(KeyCode.DownArrow) && xRotation < 90)
-        {
-            xRotation += rotationSpeed;
-        }
-        if (Input.GetKey(KeyCode.UpArrow) && xRotation > -90)
-        {
-            xRotation -= rotationSpeed;
-        }
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            yRotation -= rotationSpeed;
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            yRotation += rotationSpeed;
-        }
-
-        this.transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
     }
 }
